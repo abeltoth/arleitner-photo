437b726dd2fc46e21b0d6aecf460b92d76b4ebe4
Ábel Tóth <abel.toth@nuage.hu>
2019-12-25 04:46:46 +0100

    Navbar icons & animations

--- ui/src/app/shared/navbar/navbar.component.ts
+++ ui/src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger(
+      'inOutMobileAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('0.5s ease-out',
+                    style({ height: 370, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ height: 370, opacity: 1 }),
+            animate('0.5s ease-in',
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    ),
+    trigger(
+      'inOutDesktopAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('0.5s ease-out',
+                    style({ height: 64, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ height: 64, opacity: 1 }),
+            animate('0.5s ease-in',
+                    style({ height: 0, opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class NavbarComponent implements OnInit {
 
