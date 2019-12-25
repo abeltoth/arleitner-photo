e834e314d660f1d46e6800648bd2e0c036a2b053
Ábel Tóth <abel.toth@nuage.hu>
2019-12-25 02:24:37 +0100

    Navbar added

--- ui/src/app/home/home/home.module.ts
+++ ui/src/app/home/home/home.module.ts
@@ -1,11 +1,10 @@
+import { SharedModule } from './../../shared/shared.module';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -15,8 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     HomeRoutingModule,
     NgbCarouselModule,
-    MatIconModule,
-    MatButtonModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
--- ui/src/app/shared/shared.module.ts
+++ ui/src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
-import { AppRoutingModule } from './../app-routing.module';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell/shell.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -16,8 +19,12 @@ import { NgxSpinnerModule } from 'ngx-spinner';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule,
     NgxSpinnerModule,
+    RouterModule,
+    TranslateModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     ShellComponent,
